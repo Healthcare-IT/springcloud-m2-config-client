27c807a811b734a11909fe93e2dd1bfb09cfbcca
Healthcare IT <healthcare.it@outlook.com>
2018-12-18 13:12:12 -0500

    Initial commit

---
+++ src/main/java/io/github/healthcareit/demo/config/SpringcloudM2ConfigClientApplication.java
@@ -0,0 +1,14 @@
+package io.github.healthcareit.demo.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudM2ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudM2ConfigClientApplication.class, args);
+	}
+
+}
+
